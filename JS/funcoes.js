688d63560c7fd276eeb487f80dd43cba656f6944
lipe-ads <felipemarinhofernandess@gmail.com>
2025-06-26 17:29:45 -0300

    Adição do arquivo funções em .js

---
+++ JS/funcoes.js
@@ -0,0 +1,86 @@
+let linha = '--------------------------------------------';
+console.log(linha);
+
+/*// Function
+
+//   Funções servem para facilitar,automatizar, diminuir nossos codigos e etc
+
+//   Aqui esta a representação de alguem entrando no meu portifolio, mas eu sei (e espero) que mais de uma pessoa vai entrar nesse arquivo, então eu não posso e nem  consigo ficar atualizando a cada acesso...
+
+//   console.log("Olá Contratante 1, seja bem vindo ao meu estilo de aprendizado sobre funções!")
+//   console.log("Olá Contratante 2, seja bem vindo ao meu estilo de aprendizado sobre funções!")
+
+ // -----------------------------------------------------//
+
+ // Function e sua sintaxe
+
+ function //<- inicio de uma função 
+ saudacao //<- Nome da função, que geralmente tem que ser algo relacionado ao que a função vai fazer 
+(contratante) //<- Dados que a função precisa para funcionar perfeitamente  
+{
+  // //instruções do que a função tem que fazer:
+     console.log(
+         `Olá ${contratante}, seja bem vindo ao meu estilo de aprendizado sobre funções!`
+       );
+    //   //Imprima a frase com a variavel contratante no local desejado
+     }
+    
+     saudacao(); //Como chamamos a função no terminal
+    // console.log(linha);
+    // saudacao("#contratante 1");
+    // saudacao("#contratante 2");
+    // saudacao("#contratante 3");
+    // saudacao("#contratante 4");
+    // saudacao("#contratante 5");
+    // saudacao("#contratante 6");
+    
+    // Vai dar um underfind pq ainda não define uma variavel com o nome contratante, mas tem formas mas facieis de fazer isso*/
+    
+/*    // -----------EXEMPLOS ---------------//
+    
+    function mensagemInformeNome() {
+  console.log(
+    'Para continuar você precisa inforamar o seu nome para cadastro!'
+  );
+}
+// mensagemInformeNome()
+
+//Ex cadastro de usuarios
+
+function cadastro(nome, sobrenome) {
+  console.log(`Olá ${nome} ${sobrenome}, você foi cadastrado com sucesso!`);
+}
+// cadastro()
+// console.log(linha)
+
+// Ex Operações matematicas
+
+function banco(deposito, saque) {
+  let saldo = deposito - saque;
+  return saldo;
+}
+// console.log(banco(1000, 60))
+
+// Função Principal
+
+function main() {
+  mensagemInformeNome();
+  cadastro('Felipe', 'Marinho');
+  let saldo = banco(1000, 60);
+  console.log(`O saldo atual da sua conta é de ${saldo} reais`);
+}
+
+main();
+console.log(linha);*/
+
+/*// Chamadas de funções
+
+function exibirDetalhesProf (nome, curso){
+  console.log(`Professor: ${nome}. Curso:${curso}`)
+}
+
+exibirDetalhesProf() //Para chamar função e caso não colocarmos nenhum dos dados pedidos dentro do parenteses ira aparecer o Underfind.
+// Se não pedir dado, não precisa colocar nada entre parenteses
+exibirDetalhesProf("Fulano", "ADS")
+console.log(linha);*/
+
